350547e9164fd2968a4c91e67620325638d0f802
Metin <me@my>
2025-08-08 09:19:27 +0100

    2: Draw player

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
@@ -7,12 +8,22 @@ def main():
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    Clock = pygame.time.Clock()
+    dt = 0
+
+    # instantiate player
+    player_one = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
+
     while 1 > 0:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill("black")
+        player_one.draw(screen)
         pygame.display.flip()
+        dt = Clock.tick(60)/1000
+
+    
 
 if __name__ == "__main__":
     main()
