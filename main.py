461ad32e11978b1b1566621c5a6161809fd01bf6
Metin <me@my>
2025-08-08 13:30:53 +0100

    5: Add Shooting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from constants import *
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
-
+from shot import Shot
 
 def main():
     pygame.init()
@@ -18,11 +18,14 @@ def main():
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
     asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
+
 
     Player.containers = (updatable, drawable)
     Asteroid.containers = (asteroids, updatable, drawable)
     AsteroidField.containers = (updatable)
     Field = AsteroidField()
+    Shot.containers = (updatable, drawable)
 
     # instantiate player
     player_one = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2, PLAYER_RADIUS)
@@ -49,6 +52,9 @@ def main():
                 event.type == pygame.QUIT
                 return print("Game Over!")
             
+        for bullets in shots:
+            bullets.draw(screen)
+
         pygame.display.flip()
         dt = Clock.tick(60)/1000
 
--- player.py
+++ player.py
@@ -1,7 +1,7 @@
 import pygame
 from circleshape import CircleShape
-from constants import PLAYER_TURN_SPEED, PLAYER_SPEED
-
+from constants import PLAYER_TURN_SPEED, PLAYER_SPEED, PLAYER_SHOOT_SPEED, SHOT_RADIUS
+from shot import Shot
 
 class Player(CircleShape):
     def __init__(self, x, y, radius):
@@ -34,7 +34,15 @@ class Player(CircleShape):
             self.move(dt)
         if keys[pygame.K_s]:
             self.move(-dt)
+        if keys[pygame.K_SPACE]:
+            self.shoot(dt)
     
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self, dt):
+        bullet = Shot(self.position.x, self.position.y, SHOT_RADIUS)
+        bullet.velocity = pygame.Vector2(0, PLAYER_SHOOT_SPEED).rotate(self.rotation)
+        bullet.forward = pygame.Vector2(0, 1).rotate(self.rotation)
+        bullet.position += bullet.forward * PLAYER_SHOOT_SPEED * dt
